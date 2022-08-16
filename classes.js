d25255378d8cb9be79550b9c11e7b9b83a8e883e
Jonathan Vegas <jonathanvegas@gmail.com>
2022-08-15 21:52:19 -0400

    added more info

---
+++ classes.js
@@ -0,0 +1,27 @@
+class Human {
+  name = ""
+  constructor(name) {
+      this.name = name
+  }
+  sayHi() {
+      return `my name is ${this.name}! `
+  }
+}
+
+class Teacher extends Human {
+  currentlyTeaching = "yes"
+  sayHi() {  // overriding old sayHi
+      return `Hi, I'm professor ${this.name}! `
+  }
+}
+
+class Student extends Human {
+  grade = 100
+}
+
+let student1 = new Student("Bobby")
+let teacher = new Teacher("Jennifer")
+
+console.log(`teacher's anme is: ${teacher.name}`)
+console.log(teacher.sayHi())
+console.log(student1.sayHi())
